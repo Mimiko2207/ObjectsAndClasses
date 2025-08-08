2f586205e6c6f42311947e779798141ba98336a2
Mimiko2207 <loparev.m@mail.ru>
2025-08-08 17:56:24 +0700

    ObjectsAndClasses ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Book crime = new Book("Crime","Den Maxwell", "1980");
+        Author author = new Author("Den", "Maxwell");
     }
 }
